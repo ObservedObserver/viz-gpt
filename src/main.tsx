9f2ebc9f7270b8be36ba97262a7708451b76ab25
jojocys <270001151@qq.com>
2023-05-11 11:27:30 +0800

    feat: add va track

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import { inject } from '@vercel/analytics';
- 
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { inject, track } from "@vercel/analytics";
+
 inject();
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+track("pageview", {
+    path: window.location.href,
+});
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
